22b321c6cd83b6cb3b6f00b9120c48fa868a4a04
Ihor Drul <druleger@gmail.com>
2021-01-28 16:12:07 +0200

    add auto refresh button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,32 +10,57 @@ export class App extends React.Component {
     this.state = {
       items: [],
       isLoading: false,
+      enableAutoRefresh: false,
     }
   }
 
   componentDidMount() {
+    this.getItems()
+  }
+
+  getItems = () => {
     this.setState({
       isLoading: true
     })
     fetch('https://www.reddit.com/r/reactjs.json?limit=10')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          items: data.data.children,
-          isLoading: false
-        })
+    .then(response => response.json())
+    .then(({data}) => {
+      this.setState({
+        items: data.children,
+        isLoading: false
       })
+    })
+  }
+
+  updateAutoRefresh = () => {
+    if(this.state.enableAutoRefresh) {
+      this.setState({enableAutoRefresh: false})
+      clearInterval(this.autoRefresh)
+    }else{
+      this.setState({enableAutoRefresh: true})
+      this.autoRefresh = setInterval(this.getItems, 3000)
+    }
   }
 
   render() {
-    const {items, isLoading } = this.state
+    const {items, isLoading, enableAutoRefresh } = this.state
+    console.log(items.data)
+    const itemsSortByComments = items.sort(
+      (a, b) => b.data.num_comments - a.data.num_comments
+    )
     return(
       <div className="App App-logo">
         <h1>Top commended</h1>
+        <button 
+          type='button' 
+          onClick={this.updateAutoRefresh}
+        >
+          {enableAutoRefresh ? 'Stop' : 'Start'} auto-refresh
+        </button>
         {isLoading 
           ? <p>...Loading</p> 
           : (
-          items.map(item => <Item key={item.data.id} data={item.data}/>)
+          itemsSortByComments.map(item => <Item key={item.data.id} data={item.data}/>)
         )}
       </div>
     )

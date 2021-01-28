631195de995790c68ed67d17aa45562fe7530d8f
Ihor Drul <druleger@gmail.com>
2021-01-28 11:06:48 +0200

    fetch data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      items: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://www.reddit.com/r/reactjs.json?limit=10')
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        this.setState({
+          items: data.data.children
+        })
+      })
+  }
+
+  render() {
+    const {items} = this.state
+    return(
+      <div className="App App-logo">
+        <h1>Top commended</h1>
+        {items.map(item => (
+          <div key={item.data.id} className={'post-sec'}>
+            { item.data.thumbnail !== 'self' ? <img src={item.data.thumbnail} alt='my image'/> : null }
+            <p>{item.data.title}</p>
+            <p>{item.data.num_comments}</p>
+            <a href={item.data.permalink} target='_blank'>link</a>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
-
-export default App;
